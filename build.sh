f708b6e8cd342e11e6c04e5b3ae8f768177929a1
jamestaylr <jamestaylr@gmail.com>
2015-04-20 22:52:54 -0400

    Setup project configuration in build script

--- build.sh
+++ build.sh
@@ -1,13 +1,33 @@
+print_message() {
+	echo '\033[36m\033[1m'$1'\033[0m\033[39m'
+}
+
+print_message 'Updating Raspberry Pi and installing packages...'
 sudo su # Make the current session run as root
+cd ~/ # Move the session to the home directory
+apt-get install git
 apt-get update
 apt-get upgrade
+
+echo 'pi:qLRVS5jAcua43RJw' | chpasswd # Set the password of the Pi to a randomly generated String
 
 # Install Node JS
 wget http://node-arm.herokuapp.com/node_latest_armhf.deb
 sudo dpkg -i node_latest_armhf.deb
 
-npm install # Install all of the Node JS dependencies
-echo 'pi:qLRVS5jAcua43RJw' | chpasswd # Set the password of the Pi to a randomly generated String
+print_message 'Mounting the external drive...'
+mount -o uid=pi,gid=pi /dev/sda1 /media/usbhdd/
+cd /media/usbhdd
 
+print_message 'Setting up the project files...'
+mkdir server
+cd server
+
+git clone https://github.com/jamestaylr/file-server.git
+
+print_message 'Installing Node dependencies...'
+npm install # Install all of the Node JS dependencies
+
+print_message 'Starting the server...'
 # Start the server
 forever start server.js
